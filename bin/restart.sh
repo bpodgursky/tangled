38d242f83a1bd433f25de86b748310a15d1e9187
Ben Podgursky <bpodgursky@liveramp.com>
2017-12-01 12:20:27 -0800

    match expected jar pattern

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,4 +3,4 @@
 pkill -9 com.liveramp.tangled.WebServer
 
 mkdir log
-java -cp tangled-1.0-SNAPSHOT-uber.jar com.liveramp.tangled.WebServer config.json 2>> log/tangled.err 1>> log/tangled.out &
+java -cp tangled.job.jar com.liveramp.tangled.WebServer config.json 2>> log/tangled.err 1>> log/tangled.out &
