4e45fdde0ed85296eb97c8ba9e5fd080e75ac396
Ben Podgursky <bpodgursky@liveramp.com>
2017-12-01 10:42:05 -0800

    simple restart script

--- bin/restart.sh
+++ bin/restart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+pkill -9 com.liveramp.tangled.WebServer
+
 mkdir log
 java -cp tangled-1.0-SNAPSHOT-uber.jar com.liveramp.tangled.WebServer config.json 2>> log/tangled.err 1>> log/tangled.out &
