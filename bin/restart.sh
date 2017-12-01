9af7e7e4ee4733274158e2a7b09b802cd7ac6a3d
Ben Podgursky <bpodgursky@liveramp.com>
2017-12-01 12:27:23 -0800

    expect diff config

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,4 +3,4 @@
 pkill -9 com.liveramp.tangled.WebServer
 
 mkdir log
-java -cp tangled.job.jar com.liveramp.tangled.WebServer config.json 2>> log/tangled.err 1>> log/tangled.out &
+java -cp tangled.job.jar com.liveramp.tangled.WebServer config/config.json 2>> log/tangled.err 1>> log/tangled.out &
