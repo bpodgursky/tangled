85669986f25d1abb851331e09f324f12c6205ab8
Ben Podgursky <bpodgursky@liveramp.com>
2017-12-13 11:52:44 -0800

    tmp dir where it won't be tmpwatched

--- bin/restart.sh
+++ bin/restart.sh
@@ -3,4 +3,4 @@
 pkill -9 -f com.liveramp.tangled.WebServer
 
 mkdir -p log
-java -cp tangled.job.jar com.liveramp.tangled.WebServer config/config.json 2>> log/tangled.err 1>> log/tangled.out &
+java -Djava.io.tmpdir=/var/www/tmp  -cp tangled.job.jar com.liveramp.tangled.WebServer config/config.json 2>> log/tangled.err 1>> log/tangled.out &
